b2f73b0a1c060e096ed13253ba6156d7ab8a3511
Kurniawan <kurnhyalcantara@gmail.com>
2023-10-23 09:15:31 +0800

    init: setup database postgres connection

--- app/config/db.go
+++ app/config/db.go
@@ -13,6 +13,10 @@ type DBConfig struct {
 	DB_USERNAME	string
 	DB_PASS	string
 	DB_NAME string
+	DB_SSLMODE string
+	DB_MAXOPENCONN int
+	DB_MAXIDLECONN int
+	DB_MAXLIFETIMECONN int
 }
 
 func LoadDBConfig() (*DBConfig, error) {
@@ -21,8 +25,12 @@ func LoadDBConfig() (*DBConfig, error) {
 	dbUsername, foundDBUsername := os.LookupEnv("DB_USERNAME")
 	dbPass, foundDBPass := os.LookupEnv("DB_PASS")
 	dbName, foundDBName := os.LookupEnv("DB_NAME")
+	dbSslMode, foundDBSslMode := os.LookupEnv("DB_SSLMODE")
+	dbMaxOpenConn, foundDBMaxOpenConn := os.LookupEnv("DB_MAXOPENCONN")
+	dbMaxIdleConn, foundDBMaxIdleConn := os.LookupEnv("DB_MAXIDLECONN")
+	dbMaxLifeTimeConn, foundDBMaxLifeTimeConn := os.LookupEnv("DB_MAXLIFETIMECONN")
 
-	if !foundDBHost || !foundDBPort || !foundDBUsername || !foundDBPass || !foundDBName {
+	if !foundDBHost || !foundDBPort || !foundDBUsername || !foundDBPass || !foundDBName || foundDBSslMode || foundDBMaxOpenConn || foundDBMaxLifeTimeConn || foundDBMaxIdleConn {
 		viper.AddConfigPath(".")
 		viper.SetConfigName(".env.local")
 		viper.SetConfigType("env")
@@ -32,9 +40,16 @@ func LoadDBConfig() (*DBConfig, error) {
 		dbUsername = viper.GetString("DB_USERNAME")
 		dbPass = viper.GetString("DB_PASS")
 		dbName = viper.GetString("DB_NAME")
+		dbSslMode = viper.GetString("DB_SSLMODE")
+		dbMaxOpenConn = viper.GetString("DB_MAXOPENCONN")
+		dbMaxIdleConn = viper.GetString("DB_MAXIDLECONN")
+		dbMaxLifeTimeConn = viper.GetString("DB_MAXLIFETIMECONN")
 	}
 
 	dbPortInt, _ := strconv.Atoi(dbPort)
+	dbMaxOpenConnInt, _ := strconv.Atoi(dbMaxOpenConn)
+	dbMaxIdleConnInt, _  := strconv.Atoi(dbMaxIdleConn)
+	dbMaxLifeTimeConnInt, _ := strconv.Atoi(dbMaxLifeTimeConn)
 
 	return &DBConfig{
 		DB_HOST: dbHost,
@@ -42,5 +57,9 @@ func LoadDBConfig() (*DBConfig, error) {
 		DB_USERNAME: dbUsername,
 		DB_PASS: dbPass,
 		DB_NAME: dbName,
+		DB_SSLMODE: dbSslMode,
+		DB_MAXOPENCONN: dbMaxOpenConnInt,
+		DB_MAXIDLECONN: dbMaxIdleConnInt,
+		DB_MAXLIFETIMECONN: dbMaxLifeTimeConnInt,
 	}, nil
 }
---
+++ app/database/postgresql/postgresql.go
@@ -0,0 +1,46 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/kurnhyalcantara/Jubelio-Chat-Peer-to-Peer-API/app/config"
+)
+
+type DB struct {
+	*sql.DB
+}
+
+var postgresql = &DB{}
+
+func (db *DB) connect(dbConfig *config.DBConfig) (err error) {
+	dbURI := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		dbConfig.DB_HOST,
+		dbConfig.DB_PORT,
+		dbConfig.DB_USERNAME,
+		dbConfig.DB_PASS,
+		dbConfig.DB_NAME,
+		dbConfig.DB_SSLMODE,
+	)
+
+	postgresql.DB, err = sql.Open("postgresql", dbURI)
+	if err != nil {
+		return err
+	}
+
+	db.SetMaxOpenConns(dbConfig.DB_MAXOPENCONN)
+	db.SetMaxIdleConns(dbConfig.DB_MAXIDLECONN)
+	db.SetConnMaxLifetime(time.Duration(dbConfig.DB_MAXLIFETIMECONN))
+
+	if err := db.Ping(); err != nil {
+		defer db.Close()
+		return fmt.Errorf("can't send ping to database: %v", err)
+	}
+
+	return nil
+}
+
+func ConnectDB() error {
+	return postgresql.connect(&config.DBConfig{})
+}
